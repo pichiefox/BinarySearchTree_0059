941966bc1c06b7f8be1c66811ad46d46d3142cfb
pichiefox <citrazad@gmail.com>
2026-06-11 19:27:34 +0700

    implement inorder traversal

--- searchTree.cpp
+++ searchTree.cpp
@@ -78,3 +78,18 @@ class BinaryTree
          }
     }
 
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
