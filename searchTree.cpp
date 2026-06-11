74a74e85a1e0216ba11031f389c0a46d662aa4df
pichiefox <citrazad@gmail.com>
2026-06-11 19:28:17 +0700

    implementasi preorder traversal

--- searchTree.cpp
+++ searchTree.cpp
@@ -93,3 +93,18 @@ class BinaryTree
             inorder(ptr->rightchild);
         }
     }
+    void preorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout <<"Tree is empty" <<  endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
