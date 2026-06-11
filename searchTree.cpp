1b8b18f329ef38fb5d1abf8ed255e56d60c7a570
pichiefox <citrazad@gmail.com>
2026-06-11 19:29:25 +0700

    implement postorder traversal

--- searchTree.cpp
+++ searchTree.cpp
@@ -108,3 +108,22 @@ class BinaryTree
             preorder(ptr->rightchild);
         }
     }
+    void postorder(Node *ptr)
+    {
+        // perfoms the postorder traveersal of the tree
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
+
