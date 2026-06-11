87fda250b9638dc6bc1b9faf2b18338267428429
pichiefox <citrazad@gmail.com>
2026-06-11 19:17:14 +0700

    menambahkan constructor di Node

--- searchTree.cpp
+++ searchTree.cpp
@@ -9,11 +9,12 @@ public:
     Node* leftchild;
     Node* rightchild;
 
-    // Constructor for the Node class
-    Node (string i, Node *l, Node *r)
+    // Constructor for the node class
+    Node(string i, Node *l, Node *r)
     {
         info = i;
         leftchild = l;
         rightchild = r;
     }
+
 };
